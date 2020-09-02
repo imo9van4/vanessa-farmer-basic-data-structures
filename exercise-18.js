c75100b05bea2969e3ce05cee152e5f3103dc794
Vanessa F <68393728+imo9van4@users.noreply.github.com>
2020-09-02 14:58:02 -0700

    Add files via upload

---
+++ exercise-18.js
@@ -0,0 +1,30 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+function countOnline(usersObj) {
+  // Only change code below this line
+  let result = 0;
+  for(let user in usersObj) {
+    if (usersObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+  // Only change code above this line
+}
+console.log(countOnline(users));
